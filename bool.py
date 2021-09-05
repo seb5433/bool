ad43bda86f89cb5ae239b3651d5a2ef68941e5ec
seb5433 <ca.sebastianlv@gmail.com>
2021-09-05 13:11:44 -0400

    Solución de un tema

--- bool.py
+++ bool.py
@@ -1,10 +1,9 @@
-# A program to convert from binary to hexadecimal demonstrating the actual algorithm,
-# not just using hex()
 import sys
 import math
 import utils
 import truths
 import Kmap
+import hamming
 
 
 def conversion(Bin):
@@ -63,106 +62,6 @@ def simplificar(variables, salidas_1, no_importan=[]):
     print(f"Función simplificada: {expresion} 🏆🥇")
 
 
-def hamming():
-    option = 1
-    if(option == 1):  # GENERATE HAMMING CODE
-        print('Inserte el mensaje en bits')
-        d = input()
-        data = list(d)
-        data.reverse()
-        c, ch, j, r, h = 0, 0, 0, 0, []
-
-        while ((len(d)+r+1) > (pow(2, r))):
-            r = r+1
-
-        for i in range(0, (r+len(data))):
-            p = (2**c)
-
-            if(p == (i+1)):
-                h.append(0)
-                c = c+1
-
-            else:
-                h.append(int(data[j]))
-                j = j+1
-
-        for parity in range(0, (len(h))):
-            ph = (2**ch)
-            if(ph == (parity+1)):
-                startIndex = ph-1
-                i = startIndex
-                toXor = []
-
-                while(i < len(h)):
-                    block = h[i:i+ph]
-                    toXor.extend(block)
-                    i += 2*ph
-
-                for z in range(1, len(toXor)):
-                    h[startIndex] = h[startIndex] ^ toXor[z]
-                ch += 1
-
-        h.reverse()
-        print('El codigo Hamming es: ', end="")
-        print(int(''.join(map(str, h))))
-
-    elif(option == 2):  # DETECT ERROR IN RECEIVED HAMMING CODE
-        print('Enter the hamming code received')
-        d = input()
-        data = list(d)
-        data.reverse()
-        c, ch, j, r, error, h, parity_list, h_copy = 0, 0, 0, 0, 0, [], [], []
-
-        for k in range(0, len(data)):
-            p = (2**c)
-            h.append(int(data[k]))
-            h_copy.append(data[k])
-            if(p == (k+1)):
-                c = c+1
-
-        for parity in range(0, (len(h))):
-            ph = (2**ch)
-            if(ph == (parity+1)):
-
-                startIndex = ph-1
-                i = startIndex
-                toXor = []
-
-                while(i < len(h)):
-                    block = h[i:i+ph]
-                    toXor.extend(block)
-                    i += 2*ph
-
-                for z in range(1, len(toXor)):
-                    h[startIndex] = h[startIndex] ^ toXor[z]
-                parity_list.append(h[parity])
-                ch += 1
-        parity_list.reverse()
-        error = sum(int(parity_list) * (2 ** i)
-                    for i, parity_list in enumerate(parity_list[::-1]))
-
-        if((error) == 0):
-            print('There is no error in the hamming code received')
-
-        elif((error) >= len(h_copy)):
-            print('Error cannot be detected')
-
-        else:
-            print('Error is in', error, 'bit')
-
-            if(h_copy[error-1] == '0'):
-                h_copy[error-1] = '1'
-
-            elif(h_copy[error-1] == '1'):
-                h_copy[error-1] = '0'
-                print('After correction hamming code is:- ')
-            h_copy.reverse()
-            print(int(''.join(map(str, h_copy))))
-
-    else:
-        print('Option entered does not exist')
-
-
 Opcion = input(
     "Selecciona una opción: \n 1 - Evaluar una expresión con tabla de verdad\n 2 - Simplificar una expresión con mapa de Karnaugh\n 3 - Conversión\n 4 - Hamming\n")
 if Opcion == '1':
@@ -204,4 +103,5 @@ elif Opcion == '3':
         conversion(BinaryIN)
         correct = True
 elif Opcion == '4':
-    hamming()
+    mensaje = input("Inserte el mensaje a traducir: ")
+    hamming.hamming(mensaje)
--- hamming.py
+++ hamming.py
@@ -1,71 +1,85 @@
-mensaje = '0110'
-finish = False
-hamming = []
-count = 1
 
-paridad_pos = 1
-pos = 1
-pos_message = 0
-while not finish:
-    if pos == paridad_pos:
-        # print(paridad_pos)
-        paridad_pos = 2 ** count
-        count += 1
-        hamming.append('X')
+def hamming(mensaje):
+    # Función que retorna la conversión de un mensaje a codigo hamming
+    mensaje = mensaje
+    finish = False
+    hamming = []
+    count = 1
 
-    else:
-        hamming.append(mensaje[pos_message])
-
-        if pos_message == len(mensaje)-1:
-            finish = True
-        pos_message += 1
-
-    pos += 1
-
-cant_paridad = count-1
-
-
-codigo = []
-for x in range(0, cant_paridad):
-    # print(x)
+    paridad_pos = 1
     pos = 1
-    bajados = []
-    cant_1 = 0
-    cant_0 = 0
-    for y in hamming:
-        bin_pos = []
-        # print(bin(pos))
-        for z in range(len(bin(pos))-1, 0, -1):
-            bin_pos.append(bin(pos)[z])
-        #print(bin_pos, "---", bin(pos))
-        try:
-            if bin_pos[x] == '1' and y != 'X':
-                if y == '1':
-                    cant_1 += 1
-                else:
-                    cant_0 += 1
-                bajados.append(y)
-                #print(f"Bajar {y}, en la {x} vuelta y posicion {bin(pos)}")
-        except:
-            pass
+    pos_message = 0
+    while not finish:
+        if pos == paridad_pos:
+            # print(paridad_pos)
+            paridad_pos = 2 ** count
+            count += 1
+            hamming.append('X')
+
+        else:
+            hamming.append(mensaje[pos_message])
+
+            if pos_message == len(mensaje)-1:
+                finish = True
+            pos_message += 1
 
         pos += 1
 
-    if cant_1 % 2 != 0:
-        #print(f"Uno de paridad", end=" ")
-        codigo.append("1")
-    else:
-        #print("Cero de paridad", end=" ")
-        codigo.append("0")
+    cant_paridad = count-1
 
-    #print(f"{bajados} contiene {cant_1} unos y {cant_0} ceros")
-print(codigo, end="  ")
-print(hamming)
-count = 0
-count_1 = 0
-for x in hamming:
-    if x == "X":
-        hamming[count] = codigo[count_1]
-        count_1 += 1
-    count += 1
-print(hamming)
+    codigo = []
+    for x in range(0, cant_paridad):
+        # print(x)
+        pos = 1
+        bajados = []
+        cant_1 = 0
+        cant_0 = 0
+        for y in hamming:
+            bin_pos = []
+            # print(bin(pos))
+            for z in range(len(bin(pos))-1, 0, -1):
+                bin_pos.append(bin(pos)[z])
+            #print(bin_pos, "---", bin(pos))
+            try:
+                if bin_pos[x] == '1' and y != 'X':
+                    if y == '1':
+                        cant_1 += 1
+                    else:
+                        cant_0 += 1
+                    bajados.append(y)
+                    #print(f"Bajar {y}, en la {x} vuelta y posicion {bin(pos)}")
+            except:
+                pass
+
+            pos += 1
+
+        if cant_1 % 2 != 0:
+            #print(f"Uno de paridad", end=" ")
+            codigo.append("1")
+        else:
+            #print("Cero de paridad", end=" ")
+            codigo.append("0")
+
+        #print(f"{bajados} contiene {cant_1} unos y {cant_0} ceros")
+    #print(codigo, end="  ")
+    # print(hamming)
+    count = 0
+    count_1 = 0
+    for x in hamming:
+        if x == "X":
+            hamming[count] = codigo[count_1]
+            count_1 += 1
+        count += 1
+    # print(hamming)
+    code = ''
+    for x in codigo:
+        code = code + x
+    hamm = ''
+    for x in hamming:
+        hamm = hamm + x
+
+    print(f"Mensaje: \t{mensaje}\nCodigo: \t{code}\nHamming: \t{hamm}")
+
+
+if __name__ == "__main__":
+    hamming('1011100110011')
